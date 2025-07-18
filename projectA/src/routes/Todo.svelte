42ada558e501390bd5602ca2320bf32e9262c617
timothe_chauvet <timothe_chauvet@ext.carrefour.com>
2025-07-18 22:37:39 +0200

    :sparkles: First commit but i didnt make it work yet

---
+++ projectA/src/routes/Todo.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let task;
+  export let completed = false;
+</script>
+
+<p>
+  <input type="checkbox" checked={completed}>
+  {task}
+</p>
---
+++ projectA/src/routes/TodoList.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Todo from "./Todo.svelte";
+    let todos = [
+        { task: "Mow lawn", completed: false },
+        { task: "Garer le chien", completed: false },
+        { task: "Promener la voiture", completed: false },
+    ]
+</script>
+
+{#each todos as todo}
+    <Todo {...todo} />
+{/each}
+
+{#if todos.length === 0}
+  <p>All done!</p>
+{:else if todos.length === 1}
+  <p>Almost there!</p>
+{:else}
+  <p>Keep going!</p>
+{/if}
